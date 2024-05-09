6595ffc8fcba5248cb573f59f1b61c9d96bd21de
Darlan Alves <me@darlanalv.es>
2024-05-09 17:36:49 +0200

    feat: add speech with language

--- components/App.vue
+++ components/App.vue
@@ -3,8 +3,10 @@
     <Flip
       :cards="deck"
       :speech="canSpeak"
+      :favorites="true"
       :language="quizSource?.language"
       @speak="onSpeak($event)"
+      @save="onSave($event)"
       @shuffle="onShuffle()"
     />
   </div>
@@ -26,9 +28,8 @@ type FlipCard = {
   back: string;
 };
 
-const uid = new URL(location.href).searchParams.get('id') || '';
 const [canSpeak, speak] = useSpeech();
-const [quizSource, refreshCards] = useData<QuizSource>(`/assets/${uid || 'nl-words'}.json`);
+const [quizSource, refreshCards] = useData<QuizSource>(`/assets/nl-words.json`);
 const deck = ref<FlipCard[]>([]);
 
 function onShuffle() {
@@ -42,8 +43,21 @@ function onSpeak({ text, language }) {
   speak(text, language);
 }
 
+function onSave({ front, back }) {
+  fetch('/api/fav/' + [front, back].join(','), { method: 'POST' });
+}
+
+async function onLoad() {
+  const uid = new URL(location.href).searchParams.get('id') || '';
+  if (uid) {
+    const req = await fetch('/api/deck/' + uid);
+    quizSource.value = await req.json();
+  }
+}
+
 onMounted(async () => {
   await refreshCards();
+  await onLoad();
   onShuffle();
 });
 </script>
--- components/Flip.vue
+++ components/Flip.vue
@@ -16,7 +16,7 @@
         </div>
       </div>
       <div class="flipper__nav">
-        <button @click="prevCard()" class="btn btn-mini">
+        <button @click="prevCard()" class="btn">
           <svg
             class="w-12 h-12"
             alt="Previous"
@@ -32,7 +32,7 @@
           </svg>
         </button>
 
-        <button @click="shuffle()" class="btn btn-mini">
+        <button @click="shuffle()" class="btn">
           <svg
             class="w-12 h-12"
             alt="Shuffle"
@@ -48,7 +48,7 @@
           </svg>
         </button>
 
-        <button v-if="speech" @click="speak()" class="btn btn-mini">
+        <button v-if="speech" @click="speak()" class="btn">
           <svg
             class="w-12 h-12"
             alt="Speak"
@@ -63,7 +63,22 @@
           </svg>
         </button>
 
-        <button @click="nextCard()" class="btn btn-mini">
+        <button v-if="favorites" @click="save()" class="btn">
+          <svg
+            class="w-12 h-12"
+            alt="Save"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path
+              d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
+            />
+          </svg>
+        </button>
+
+        <button @click="nextCard()" class="btn">
           <svg
             class="w-12 h-12"
             alt="Next"
@@ -92,6 +107,7 @@ const showTranslation = ref(false);
 
 const props = defineProps({
   speech: { type: Boolean },
+  favorites: { type: Boolean },
   language: { type: String, default: 'en' },
   cards: {
     type: Array,
@@ -99,11 +115,17 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['shuffle', 'speak']);
+const emit = defineEmits(['shuffle', 'speak', 'save']);
 
 function haveValidCards() {
   return props.cards?.length && Array.isArray(props.cards);
 }
+
+function save() {
+  const card = props.cards[index.value];
+  emit('save', card);
+}
+
 function shuffle() {
   animated.value = false;
   unflip();
@@ -156,11 +178,11 @@ function nextCard() {
 }
 
 function handleKeyDown(event) {
-  if (event.key === 'd' || event.key === 'ArrowLeft') {
+  if (event.key === 'a' || event.key === 'ArrowLeft') {
     prevCard();
   }
 
-  if (event.key === 'f' || event.key === 'ArrowRight') {
+  if (event.key === 'd' || event.key === 'ArrowRight') {
     nextCard();
   }
 
@@ -168,12 +190,16 @@ function handleKeyDown(event) {
     flip();
   }
 
-  if (event.key === 'a' || event.key === 'ArrowUp') {
+  if (event.key === 'w' || event.key === 'ArrowUp') {
     unflip();
   }
 
   if (event.key === 'r') {
     shuffle();
+  }
+
+  if (event.key === 'q') {
+    save();
   }
 
   if (event.key === 'p' && props.speech) {
