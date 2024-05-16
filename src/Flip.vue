ed63eb19fbf6c5dccc4496ec18cd06cdb851904f
Darlan Alves <me@darlanalv.es>
2024-05-16 10:25:30 +0200

    chore: enhance favorite button

--- src/Flip.vue
+++ src/Flip.vue
@@ -80,7 +80,7 @@
             height="24"
             width="24"
             alt="Save"
-            viewBox="-2 -2 26 26"
+            viewBox="-1 -1 26 26"
             class="btn__icon"
             >
             <path style="stroke:currentColor" fill="none" stroke-width="2" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
